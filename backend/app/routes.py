d98aee714dd3d534cdc83dd5ee00911b2bf546d4
Esha Shah <67078410+Eshah7@users.noreply.github.com>
2024-06-27 17:41:48 -0400

    created api call

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,3 +5,28 @@ main = Blueprint('main', __name__)
 @main.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello from Flask!")
+
+@main.route('/api/search', methods=['GET'])
+def search():
+    
+    api_key = '94def37c570df053630ad99518df19b761fbf611a3157874339cc8230a787d05'  # Replace with your SerpAPI key
+    search_url = f'https://serpapi.com/search'
+    
+    params = {
+        'api_key': api_key,
+        "engine": "google_flights",
+        "hl": "en",
+        "gl": "us",
+        "departure_id": "YYZ", #User Inputs
+        "arrival_id": "LAS", #User Inputs 
+        "outbound_date": "2024-08-12", #This data will be user inputted. 
+        "return_date": "2024-08-20",
+        "currency": "CAD"
+    }
+    
+    response = requests.get(search_url, params=params)
+    
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'Failed to retrieve data from SerpAPI', 'status_code': response.status_code})
