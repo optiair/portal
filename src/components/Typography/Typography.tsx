f1a0c53538db25464a6b7d10cac3daca86c049e2
Safewaan <safwaanchowdhury26@gmail.com>
2024-07-03 03:55:53 -0400

    setup linter and create Typography

---
+++ src/components/Typography/Typography.tsx
@@ -0,0 +1,44 @@
+import './Typography.scss';
+import { ReactNode } from 'react';
+
+type FontVariant =
+  | 'tiny'
+  | 'small'
+  | 'small-medium'
+  | 'medium'
+  | 'medium-large'
+  | 'large'
+  | 'extra-large';
+
+interface TypographyProps {
+  id?: string;
+  variant: FontVariant;
+  color?: string;
+  children:
+    | string
+    | string[]
+    | JSX.Element
+    | JSX.Element[]
+    | number
+    | undefined
+    | null
+    | (string | JSX.Element)[]
+    | ReactNode;
+}
+
+export const Typography: React.FC<TypographyProps> = ({
+  id,
+  variant,
+  color,
+  children,
+}) => {
+  return (
+    <p
+      id={id}
+      className={variant}
+      style={{ color }}
+    >
+      {children}
+    </p>
+  );
+};
