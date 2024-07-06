1a535d608fd8039983a54e3a93ae4fb3bfb18d26
Safwaan Chowdhury <57375371+Safewaan@users.noreply.github.com>
2024-07-06 01:28:19 -0400

    Finish Backend (#5)
    * add search arg validation
    
    * be
    
    * Delete .vscode/settings.json

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,40 +1,65 @@
-from flask import Blueprint, jsonify
 from dotenv import load_dotenv
+from flask import Blueprint, jsonify, request
+from datetime import datetime
+from werkzeug.exceptions import BadRequest
 import requests
 import os
+
+from app.util import process_flight_data, calculate_scores
 
 # Load environment variables from .env file
 load_dotenv()
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/api/hello', methods=['GET'])
+
+@main.route("/api/hello", methods=["GET"])
 def hello():
     return jsonify(message="Hello from Flask!")
 
-@main.route('/api/search', methods=['GET'])
+
+@main.route("/api/search", methods=["GET"])
 def search():
-    api_key = os.getenv('SERPAPI_KEY')
-    search_url = f'https://serpapi.com/search'
-    
+    api_key = os.getenv("SERPAPI_KEY")
+    search_url = f"https://serpapi.com/search"
+
+    departure_id = request.args["departure_id"]
+    arrival_id = request.args["arrival_id"]
+    outbound_date = request.args["outbound_date"]
+    return_date = request.args.get("return_date")
+    cost_preference = int(request.args.get("cost_preference"))
+    duration_preference = int(request.args.get("duration_preference"))
+    redeye_preference = int(request.args.get("redeye_preference"))
+
+    preferences = {
+        "cost_preference": cost_preference,
+        "duration_preference": duration_preference,
+        "redeye_preference": redeye_preference,
+    }
+
     params = {
-        'api_key': api_key,
+        "api_key": api_key,
         "engine": "google_flights",
         "hl": "en",
         "gl": "us",
-        "departure_id": "YYZ", #User Inputs
-        "arrival_id": "LAS", #User Inputs 
-        "outbound_date": "2024-08-12", #This data will be user inputted. 
-        "return_date": "2024-08-20",
-        "currency": "CAD"
+        "departure_id": departure_id,
+        "arrival_id": arrival_id,
+        "type": 1 if return_date is not None else 2,
+        "outbound_date": outbound_date,
+        "return_date": return_date,
+        "currency": "CAD",
     }
-    
-    response = requests.get(search_url, params=params)
-    
-    if response.status_code == 200:
-        flight_data = response.json()
-        return jsonify(response.json())
-    else:
-        return jsonify({'error': 'Failed to retrieve data from SerpAPI', 'status_code': response.status_code})
-    
 
+    try:
+        response = requests.get(search_url, params=params)
+    except Exception as e:
+        return jsonify(
+            {
+                "error": "Failed to retrieve data from SerpAPI",
+                "status_code": response.status_code,
+            }
+        )
+
+    flight_data = process_flight_data(response.json())
+    scored_flight_data = calculate_scores(flight_data, preferences)
+    return jsonify(scored_flight_data)
--- backend/app/util.py
+++ backend/app/util.py
@@ -1,67 +1,86 @@
-import json
 from datetime import datetime, timedelta
 
-#Function Purpose: processes the data from the SerpAPI
-#Ensures that the duration time can be calculated with, and determine if a flight is classified as a "Red-Eye"
+
+# Function Purpose: processes the data from the SerpAPI
+# Ensures that the duration time can be calculated with, and determine if a flight is classified as a "Red-Eye"
 def process_flight_data(flight_data):
     processed_data = []
-    
-    if 'best_flights' in flight_data:
-        for best_flight in flight_data['best_flights']:
-            for flight in best_flight['flights']:
-                #Adjust the time, because it is coming from a string!!
-                departure_time = datetime.strptime(flight['departure_airport']['time'], '%Y-%m-%d %H:%M')
-                arrival_time = datetime.strptime(flight['arrival_airport']['time'], '%Y-%m-%d %H:%M')
-                
-                #Fix for overnight flights
+
+    if "best_flights" in flight_data:
+        for best_flight in flight_data["best_flights"]:
+            for flight in best_flight["flights"]:
+                # Adjust the time, because it is coming from a string!!
+                departure_time = datetime.strptime(
+                    flight["departure_airport"]["time"], "%Y-%m-%d %H:%M"
+                )
+                arrival_time = datetime.strptime(
+                    flight["arrival_airport"]["time"], "%Y-%m-%d %H:%M"
+                )
+
+                # Fix for overnight flights
                 if arrival_time < departure_time:
                     arrival_time += timedelta(days=1)
-                
+
                 duration = (arrival_time - departure_time).total_seconds() / 60
-                
+
                 is_redeye = departure_time.hour >= 0 and departure_time.hour < 6
-                
-                processed_data.append({
-                    'departure_airport': flight['departure_airport']['name'],
-                    'arrival_airport': flight['arrival_airport']['name'],
-                    'departure_time': departure_time,
-                    'arrival_time': arrival_time,
-                    'duration': duration,
-                    'airline': flight['airline'],
-                    'flight_number': flight['flight_number'],
-                    'price': best_flight['price'],
-                    'currency': flight_data['search_parameters']['currency'],
-                    'is_redeye': is_redeye
-                })
+
+                processed_data.append(
+                    {
+                        "departure_airport": flight["departure_airport"]["name"],
+                        "arrival_airport": flight["arrival_airport"]["name"],
+                        "departure_time": departure_time,
+                        "arrival_time": arrival_time,
+                        "duration": duration,
+                        "airline": flight["airline"],
+                        "flight_number": flight["flight_number"],
+                        "price": best_flight["price"],
+                        "currency": flight_data["search_parameters"]["currency"],
+                        "is_redeye": is_redeye,
+                    }
+                )
     return processed_data
 
-#Calculate the scores using the user's inputted preferences and the flights' comparsion with the average calculated price and duration
+
+# Calculate the scores using the user's inputted preferences and the flights' comparsion with the average calculated price and duration
 def calculate_scores(flights, preferences):
-    no_redeye_importance, price_importance, duration_importance = preferences
-    avg_price = sum(flight['price'] for flight in flights) / len(flights)
-    avg_duration = sum(flight['duration'] for flight in flights) / len(flights)
-    
+    redeye_preference, cost_preference, duration_preference = (
+        preferences["cost_preference"],
+        preferences["duration_preference"],
+        preferences["redeye_preference"],
+    )
+    avg_price = sum(flight["price"] for flight in flights) / len(flights)
+    avg_duration = sum(flight["duration"] for flight in flights) / len(flights)
+
     scores = []
     for flight in flights:
+        bin_scores = []
         score = 0
-        #Determine no_redeye_importance
-        if flight['is_redeye']:
-            score += -1 * no_redeye_importance
+        # Determine price preference score
+        if flight["price"] > avg_price:
+            bin_scores.append(-1)
+            score += -1 * cost_preference
         else:
-            score += 1 * no_redeye_importance
-        
-        #Determine price preference score
-        if flight['price'] > avg_price:
-            score += -1 * price_importance
+            bin_scores.append(1)
+            score += 1 * cost_preference
+
+        # Determine duration preference score
+        if flight["duration"] > avg_duration:
+            bin_scores.append(-1)
+            score += -1 * duration_preference
         else:
-            score += 1 * price_importance
-        
-        #Determine duration preference score
-        if flight['duration'] > avg_duration:
-            score += -1 * duration_importance
+            bin_scores.append(1)
+            score += 1 * duration_preference
+
+        flight["bin_score"] = bin_scores
+        flight["score"] = score
+
+        # Determine redeye_preference
+        if flight["is_redeye"]:
+            bin_scores.append(-1)
+            score += -1 * redeye_preference
         else:
-            score += 1 * duration_importance
-        
-        flight['score'] = score
-    
-    return {flights, avg_price, avg_duration}
+            bin_scores.append(1)
+            score += 1 * redeye_preference
+
+    return {"flights": flights, "avg_price": avg_price, "avg_duration": avg_duration}
