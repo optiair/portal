f2598df3a3f009ea3c399c4b9d8287b99af493e6
Safewaan <safwaanchowdhury26@gmail.com>
2024-07-07 01:41:05 -0400

    x

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -21,12 +21,15 @@ export const Search: React.FC = () => {
   // Airports
   const [origin, setOrigin] = useState<string>('');
   const [destination, setDestination] = useState<string>('');
+  const [airportError, setAirportError] = useState<boolean>(false);
 
   const handleOriginChange = (newValue: string) => {
+    setAirportError(newValue == destination);
     setOrigin(newValue);
   };
 
   const handleDestinationChange = (newValue: string) => {
+    setAirportError(newValue == origin);
     setDestination(newValue);
   };
 
@@ -49,6 +52,38 @@ export const Search: React.FC = () => {
     setPreferences(newPreferences);
   };
 
+  const handleGetFlights = async () => {
+    const apiUrl = 'http://127.0.0.1:5000/api/search';
+    const params = new URLSearchParams({
+      departure_id: origin,
+      arrival_id: destination,
+      outbound_date: departureDate?.toISOString().split('T')[0] || '',
+      return_date: returnDate?.toISOString().split('T')[0] || '',
+      cost_preference: preferences?.costPreference.toString() || '0',
+      duration_preference: preferences?.durationPreference.toString() || '0',
+      redeye_preference: preferences?.redeyePreference.toString() || '0',
+    });
+
+    try {
+      const response = await fetch(`${apiUrl}?${params.toString()}`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching flights:', error);
+      return null;
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const data = await handleGetFlights();
+  };
+
   return (
     <Card className={styles.card}>
       <CardHeader>
@@ -63,6 +98,11 @@ export const Search: React.FC = () => {
               From
             </Typography>
             <ComboBox combos={airports} onValueChange={handleOriginChange} />
+            {airportError && (
+              <Typography variant="small" color="#FF6347">
+                Origin and destination airports be the same.
+              </Typography>
+            )}
           </div>
           <div className={styles.searchInput}>
             <Typography variant="small" color="#549CDE">
@@ -89,7 +129,7 @@ export const Search: React.FC = () => {
         <CardFooter>
           <div className={styles.cardFooter}>
             <Preferences onPreferencesChange={handlePreferencesChange} />
-            <Button className={styles.primaryButton}>
+            <Button className={styles.primaryButton} onClick={handleSubmit}>
               <Typography variant="small">Show Flights</Typography>
             </Button>
           </div>
